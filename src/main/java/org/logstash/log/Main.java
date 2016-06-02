34100b2608d10f50f50678b2bbae9a506f868d9b
Tal Levy <jubboy333@gmail.com>
2016-06-02 15:48:14 -0700

    cool

--- src/main/java/org/logstash/log/Main.java
+++ src/main/java/org/logstash/log/Main.java
@@ -4,21 +4,14 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Main {
-    private static final Logger logger = LogManager.getLogger("HelloWorld");
+    private static final Logger logger = LogManager.getLogger(Main.class);
 
-    static class What {
-        private String ok;
-        public What(String ok) {
-            this.ok = ok;
-        }
-    }
     public static void main(String[] args) {
-        Logger logger = LogManager.getLogger("fds");
-        What ok = new What("wowza");
-        logger.info(ok);
+        ExampleEvent event = new ExampleEvent();
+        event.setField("first", Collections.singletonMap("wow", 21));
+        logger.info("hello, I am a simple structured message");
+        logger.info("hello, I am a very structured message", "event", event, "what", "hello");
     }
 }
